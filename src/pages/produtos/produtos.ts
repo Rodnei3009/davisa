3d00d199e9bc584ad17eaf05142336d8e7c4d449
Rodnei3009 <rbrassoroto@yahoo.com.br>
2017-12-23 16:57:15 -0200

    barcode
    barcode

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -59,6 +59,7 @@ export class ProdutosPage {
       .then((barcodeResult: BarcodeScanResult) => {
         this.barcodeResult = barcodeResult;
         this.codBarrasRetorno = this.barcodeResult.text;
+        this.localizar_produto();
       }).catch((error: Error) => {
         console.log('barcode error: ', error);
       });
@@ -69,7 +70,6 @@ export class ProdutosPage {
   }
 
   localizar_produto() {
-    this.onGetBarcode();
     alert(this.codBarrasRetorno);
   }
 
