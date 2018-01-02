7d95cbfb6dd9257f593ceb6cf6932fb53e43290a
Rodnei3009 <rbrassoroto@yahoo.com.br>
2018-01-01 23:00:53 -0200

    incluir
    incluir

--- src/pages/pedidos/pedidos.ts
+++ src/pages/pedidos/pedidos.ts
@@ -13,13 +13,18 @@ import { Pedido } from './../../models/pedido.models';
 })
 export class PedidosPage {
 
+  arrayProdutos = [];
+  totalItens: number = 0;
+  valorTotal: number = 0.00;
+
   clienteSelecionado: boolean = false;
   dadosCliente: Cliente;
   dadosProduto: Produto;
   nomeCliente: string = "";
   celCliente: string = "";
   dataHora: string = "";
-  detalhesPedido: Pedido;  
+  detalhesPedido: Pedido;
+
   
   constructor(public navCtrl: NavController, public navParams: NavParams, public modal: ModalController, public pedido: PedidoServiceProvider) {}
 
@@ -52,9 +57,36 @@ export class PedidosPage {
         // Do things with data coming from modal, for instance :
         console.log(dataProd);
         this.dadosProduto = dataProd;
+
+        if(dataProd != "") {
+          
+          this.totalItens = this.totalItens + 1;
+          this.valorTotal = this.valorTotal + parseFloat(dataProd.valVenda);
+          this.arrayProdutos.push(dataProd);
+
+        }
+
+        //this.detalhesPedido.itens.push(dataProd);
         //this.nomeCliente = this.dadosProduto.nome;
     });
     modal.present();
+  }
+  
+  excluirItem(item): void {
+    //alert(item);
+    let i: number = 0;
+
+    for (i=0;i<this.arrayProdutos.length;i++) {
+      //alert(this.arrayProdutos[i].codBarras);
+      if (this.arrayProdutos[i].codBarras === item) {
+        
+        this.totalItens = this.totalItens - 1;
+        this.valorTotal = this.valorTotal - parseFloat(this.arrayProdutos[i].valVenda);
+        
+        this.arrayProdutos.splice(i, 1)
+      }
+    }
+    //this.arrayProdutos.splice(item);
   }
 
   criaPedido(detalhesPedido): void {
